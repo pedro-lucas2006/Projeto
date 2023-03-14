917df0ba6dc76fedc391928ae6b5e0afeb8d1699
pedro-lucas2006 <pdrlucasrocha@gmail.com>
2023-03-14 19:09:18 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+function togglemode(){
+    const html = document.documentElement
+    html.classList.toggle("light") 
+    const img = document.querySelector("#profile img")
+    if(html.classList. contains("light")) {
+
+        img.setAttribute("src","./Avatar.png")
+        
+    }else{
+        img.setAttribute("src", "./pedro.jpg") 
+    }   
+}  
